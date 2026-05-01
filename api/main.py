30bfe737b9844eb15a17060092c7ba31b1807f42
I767435 <erik.koston@sap.com>
2026-05-01 12:15:55 +0200

    change

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from db.connect import db
 from auth.verifyUser import auth_user
@@ -10,6 +11,14 @@ app = FastAPI(
     title="Vercel + FastAPI Test",
     description="Vercel + FastAPI",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
