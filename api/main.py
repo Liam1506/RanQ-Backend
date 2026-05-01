f10bbf1977d5e1938924060a2def3375904b0e83
Liam Wittig <liam.wittig@sap.com>
2026-05-01 13:05:35 +0200

    Fix cors

--- api/main.py
+++ api/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost:4321", "https://ranq.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
